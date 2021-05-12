63e3a0eb7cd5ffd364078e399aaceb0c5c60999a
miorita76 <71668122+miorita76@users.noreply.github.com>
2021-05-12 14:44:48 +0300

    added Modal Answer

--- js/script.js
+++ js/script.js
@@ -10,3 +10,22 @@ function resizeWidthOnly(a, b) {
         }
     }, a;
 }
+
+let closeWindow = document.getElementById("modal-window_close");
+// When the user clicks on (x), close the modal
+closeWindow.onclick = function () {
+    document.getElementById("modal_answer").classList.add("visually-hidden");
+}
+
+// Get the button to close the modal
+let btnClose = document.getElementById("btn-close");
+btnClose.onclick = function () {
+    document.getElementById("modal_answer").classList.add("visually-hidden");
+}
+
+// The outside user clicks of the modal window, close it
+window.addEventListener("click", function (event) {
+    if (event.target == document.getElementById("modal_answer")) {
+        document.getElementById("modal_answer").classList.add("visually-hidden");
+    }
+});
